050aaf57a8fb22b2c231bffc35db942219209869
Nick <nclehmann@tutanota.com>
2023-05-23 21:19:40 +0200

    added Anonymize.py

---
+++ Anonymize.py
@@ -0,0 +1,13 @@
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
+
+tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
+model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+
+nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+example = "My name is Jeff and I live in Berlin"
+
+ner_results = nlp(example)
+print(ner_results)
+
+input()
