c4fc08e6a959b23d0369c11ffcc2e804921f5f54
Nick <nclehmann@tutanota.com>
2023-05-29 15:06:17 +0200

    changes to translate module

--- __main__.py
+++ __main__.py
@@ -1,6 +1,13 @@
+from translate import *
 from anonymize import *
 
+text = '''El servicio al cliente fue excelente. Me atendieron de manera amable y resolvieron todas mis dudas. 
+El producto superó mis expectativas en cuanto a calidad y funcionalidad. La entrega fue puntual y sin contratiempos. 
+Estoy muy contento con mi compra y lo recomendaré a mis amigos. Si tiene alguna pregunta, puede ponerse en contacto conmigo. 
+Mi nombre es Lucas García.'''
+
 if __name__ == '__main__':
-    print("Input sentence to anonymize here:")
-    sentence = input()
-    print(anonymize(sentence))
+    # print("Input text to anonymize here:")
+    # text = input()
+    text = translateGoogle(text, "en")
+    print(anonymize(text))
--- translate.py
+++ translate.py
@@ -1,12 +1,26 @@
 from googletrans import Translator as TranslatorGoogle
-# from deepl import Translator as TranslatorDeepL
+from deepl import Translator as TranslatorDeepL
+from config import apiKeyDeepL
 
+# Google translate
 def translateGoogle(text, destinationLanguage):
     translatorGoogle = TranslatorGoogle()
-    if destinationLanguage == "en": 
-        translation = translatorGoogle.translate(text)
-    else:
-        translation = translatorGoogle.translate(text, dest=destinationLanguage)
-    print(translation.text)
+    try:
+        if destinationLanguage == "en": 
+            translation = translatorGoogle.translate(text)
+        else:
+            translation = translatorGoogle.translate(text, dest=destinationLanguage)
+    except:
+        translation = ""
+        print("Probably unsupported language symbol. No translation possible")
+    return translation.text
 
-translateGoogle("El servicio al cliente fue excelente. Me atendieron de manera amable y resolvieron todas mis dudas. El producto superó mis expectativas en cuanto a calidad y funcionalidad. La entrega fue puntual y sin contratiempos. Estoy muy contento con mi compra y lo recomendaré a mis amigos.", "en")
+# DeepL translate
+def translateDeepL(text, destinationLanguage):
+    translatorDeepL = TranslatorDeepL(apiKeyDeepL)
+    try:
+        translation = translatorDeepL.translate(text, target_lang=destinationLanguage)
+    except:
+        translation = ""
+        print("Probably unsupported language symbol. No translation possible")
+    return translation.text
